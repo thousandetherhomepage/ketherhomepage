1097d2afb0f753bcfbbde9b8d8f7f946d20f228d
Andrey Petrov <andrey.petrov@shazow.net>
2017-08-07 14:22:23 -0400

    contract: added comments, hardcoded pixel price

--- ketherhomepage.sol
+++ ketherhomepage.sol
@@ -10,7 +10,7 @@ contract KetherHomepage {
         uint width,
         uint height
     );
-    
+
     /// Publish is emitted whenever the contents of an ad is changed.
     event Publish(
         uint idx,
@@ -18,14 +18,18 @@ contract KetherHomepage {
         string image,
         bool NSFW
     );
-    
-    uint weiPixelPrice;
-    address owner;
-    
+
+    /// Price is 1 kether divided by 1,000,000 pixels
+    uint constant weiPixelPrice = 1000000000000000;
+
     /// Each grid cell represents 10 pixels.
-    uint constant pixelsPerCell = 100;
+    uint constant pixelsPerCell = 10;
+
     bool[100][100] grid;
-    
+
+    /// owner can withdraw the funds and override NSFW status of ad units.
+    address owner;
+
     struct Ad {
         address owner;
         uint x;
@@ -34,21 +38,21 @@ contract KetherHomepage {
         uint height;
         string link;
         string image;
-        
+
         /// NSFW is whether the ad is suitable for people of all
         /// ages and workplaces.
         bool NSFW;
         /// forceNSFW can be set by owner.
         bool forceNSFW;
     }
-    
+
+    /// ads are stored in an array, the id of an ad is its index in this array.
     Ad[] public ads;
-    
-    function KetherHomepage(address _owner, uint _weiPixelPrice) {
+
+    function KetherHomepage(address _owner) {
         owner = _owner;
-        weiPixelPrice = _weiPixelPrice;
     }
-    
+
     /// Ads must be purchased in 10x10 pixel blocks.
     /// Each coordinate represents 10 pixels. That is,
     ///   _x=5, _y=10, _width=3, _height=3
@@ -56,7 +60,7 @@ contract KetherHomepage {
     function buy(uint _x, uint _y, uint _width, uint _height) payable returns (uint idx) {
         uint cost = _width * _height * pixelsPerCell * weiPixelPrice;
         require(msg.value >= cost);
-        
+
         // Loop over relevant grid entries
         for(uint i=0; i<_width; i++) {
             for(uint j=0; j<_height; j++) {
@@ -67,33 +71,39 @@ contract KetherHomepage {
                 grid[_x+i][_y+j] = true;
             }
         }
-        
+
         // We reserved space in the grid, now make a placeholder entry.
         idx = ads.push(Ad(msg.sender, _x, _y, _width, _height, "", "", false, false)) - 1;
-        
+
         Buy(idx, msg.sender, _x, _y, _width, _height);
         return idx;
     }
-    
+
+    /// Publish allows for setting the link, image, and NSFW status for the ad
+    /// unit that is identified by the idx which was returned during the buy step.
+    /// The link and image must be full web3-recognizeable URLs, such as:
+    ///  - bzz://mydomain.eth/ad.png
+    ///  - https://cdn.mydomain.com/ad.png
     function publish(uint _idx, string _link, string _image, bool _NSFW) {
         Ad storage ad = ads[_idx];
         require(ad.owner == msg.sender);
         ad.link = _link;
         ad.image = _image;
         ad.NSFW = _NSFW;
-        
+
         Publish(_idx, ad.link, ad.image, ad.NSFW || ad.forceNSFW);
     }
-    
 
+    /// forceNSFW allows the owner to override the NSFW status for a specific ad unit.
     function forceNSFW(uint _idx, bool _NSFW) {
         require(msg.sender == owner);
         Ad storage ad = ads[_idx];
         ad.forceNSFW = _NSFW;
-        
+
         Publish(_idx, ad.link, ad.image, ad.NSFW || ad.forceNSFW);
     }
-    
+
+    /// withdraw allows the owner to transfer out the balance of the contract.
     function withdraw() {
         require(msg.sender == owner);
         owner.transfer(this.balance);
