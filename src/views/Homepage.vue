cf3b7be29ceef7eb2d83f5b30c02d32b4b12c973
Andrey Petrov <andrey.petrov@shazow.net>
2017-08-20 12:39:14 -0400

    grid maybe works, need more data

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,3 +1,12 @@
+<style lang="scss">
+#adGrid {
+  position: relative;
+  width: 1000px;
+  height: 1000px;
+  background: #eee;
+}
+</style>
+
 <template>
   <div>
     <h2>Million dollar homepage here.</h2>
@@ -5,15 +14,36 @@
     <p>Ready: {{accounts}}</p>
 
     <p>Num Ads: {{numAds}}</p>
-    <ul>
-      <li v-for="tile in tiles">
-        {{ tile }}
+    <ul id="adGrid">
+      <li v-for="tile in tiles" :style="tile.style">
+        <a :href="tile.link"><img :src="tile.image" /></a>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+
+function toAd(r) {
+  const ad = {
+    x: r[0].toNumber(),
+    y: r[1].toNumber(),
+    width: r[2].toNumber(),
+    height: r[3].toNumber(),
+    link: r[4],
+    image: r[5],
+    nsfw: r[6],
+  }
+  ad.style = {
+    "position": "absolute",
+    "margin-left": ad.x * 10,
+    "margin-top": ad.y * 10,
+    "width": ad.width,
+    "height": ad.height,
+  }
+  return ad;
+}
+
 export default {
   props: ["web3", "contract"],
   data() {
@@ -30,7 +60,7 @@ export default {
 
         for (let i=0; i<this.numAds; i++) {
           this.contract.getAd.call(i, function(err, res) {
-            this.tiles.push(res);
+            this.tiles.push(toAd(res));
           }.bind(this));
         }
 
