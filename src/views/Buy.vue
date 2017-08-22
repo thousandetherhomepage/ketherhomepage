3e28b02b5623479f3a487a86ab9307a78351dfb0
Andrey Petrov <andrey.petrov@shazow.net>
2017-08-22 17:36:59 -0400

    remove rogue print

--- src/views/Buy.vue
+++ src/views/Buy.vue
@@ -77,7 +77,6 @@ export default {
       const weiPrice = this.web3.toWei(this.price(ad.width, ad.height), "ether");
       const x = Math.floor(this.x/10), y = Math.floor(this.y/10), width = Math.floor(this.width/10), height = Math.floor(this.height/10);
       const account = this.$store.state.activeAccount;
-      console.log("value", weiPrice);
       this.contract.buy.sendTransaction(x, y, width, height, { value: weiPrice, from: account }, function(err, res) {
         if (err) {
           this.error = err;
