a54f15c0f7ebef2232874e220a71926ebf102f8a
Andrey Petrov <andrey.petrov@shazow.net>
2017-08-25 14:36:03 -0400

    oops forgot offline.vue

---
+++ src/Offline.vue
@@ -0,0 +1,34 @@
+<style lang="scss">
+.offline {
+  background: #fffab2;
+  width: 40em;
+  padding: 5px 10px;
+  margin: 1em 0em;
+  font-size: 0.8rem;
+
+  h3 {
+    margin: 0.5em 0 0.5em 0;
+    color: rgba(0, 0, 0, 0.5);
+    text-transform: uppercase;
+    font-size: 1em;
+  }
+  p {
+    margin: 0.5em 0;
+  }
+}
+</style>
+
+<template>
+  <div class="offline">
+    <h3>Your Browser does not support Smart Contracts</h3>
+
+    <p>
+      You're seeing a read-only view of the Ethereum blockchain. In order to
+      interact with it from your browser, download an Ethereum enabled browser
+      like
+      <a href="https://github.com/ethereum/mist/releases">Mist</a>,
+      <a href="https://ethcore.io/parity.html">Parity</a>, or install the
+      <a href="https://metamask.io">Metamask Chrome Extension</a>.
+    </p>
+  </div>
+</template>
