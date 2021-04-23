5960a5b15f4dc230bf00a3bf58a33fef09efeb3c
Andrey Petrov <andrey.petrov@shazow.net>
2021-04-23 16:30:13 -0400

    wip: Migrating tests to ethers

--- test/ketherhomepage.js
+++ test/ketherhomepage.js
@@ -1,185 +1,119 @@
-const { BN } = require("ethereumjs-util");
-
-const KetherHomepage = await ethers.getContractFactory("KetherHomepage");
+const { expect } = require('chai');
 
 describe('KetherHomepage', function(accounts) {
-  // TODO should I query the contract to make sure the above values are right?
-  const weiPixelPrice = new BN(web3.utils.toWei("0.001", "ether"));
-  const pixelsPerCell = new BN(100);
-  const oneHundredCellPrice = pixelsPerCell.mul(weiPixelPrice).mul(new BN(100));
+  let KetherHomagepage;
 
+  // TODO: should we query the contract to make sure the above values are right?
+  const weiPixelPrice = ethers.utils.parseUnits("0.001", "ether");
+  const pixelsPerCell = ethers.BigNumber.from(100);
+  const oneHundredCellPrice = pixelsPerCell.mul(weiPixelPrice).mul(100);
 
   let owner, withdrawWallet, account1, account2;
   before(async () => {
+    KetherHomepage = await ethers.getContractFactory("KetherHomepage");
+
     // Initialize
-    const accounts = await web3.eth.getAccounts()
+    const accounts = await ethers.getSigners();
     owner = accounts[0]; // this is the account we deploy as owner, see 2_deploy_contracts.js
     withdrawWallet = accounts[1];
     account1 = accounts[2];
     account2 = accounts[3];
 
-    await web3.eth.sendTransaction({
-      from: owner,
-      to: account1,
-      value: "10000000000000000000"
+    await owner.sendTransaction({
+      to: account1.getAddress(),
+      value: ethers.utils.parseEther("100.0")
     })
   })
 
-  it("should have correct constants by default", function() {
-    let KH;
-    return KetherHomepage.new(owner, withdrawWallet)
-      .then(function(instance) {
-        KH = instance;
+  let KH;
+  beforeEach(async function () {
+    KH = await KetherHomepage.deploy(owner.getAddress(), withdrawWallet.getAddress());
+  });
 
-        return KH.weiPixelPrice.call();
-      })
-      .then(function(wpp) {
-        assert.equal(weiPixelPrice.toString(), wpp.toString());
-        return KH.pixelsPerCell.call();
-      })
-      .then(function(ppc) {
-        assert.equal(pixelsPerCell.toString(), ppc.toString());
-      });
+  it("should have correct constants by default", async function() {
+    const wpp = await KH.weiPixelPrice();
+    expect(weiPixelPrice).to.equal(wpp);
+
+    const ppc = await KH.pixelsPerCell();
+    expect(pixelsPerCell).to.equal(ppc);
   });
 
   it("should be a kether", function() {
-    const fullSize = new BN('10000'); // 100 * 100
-    const amount = weiPixelPrice.mul(pixelsPerCell).mul(fullSize);
-    const kether = web3.utils.fromWei(amount, 'kether');
-    assert.equal(1, kether);
+    const amount = weiPixelPrice.mul(pixelsPerCell).mul('10000'); // 100 * 100
+    const ether = ethers.utils.formatUnits(amount, 'ether');
+    expect(ether).to.equal('1000.0'); // 1 kether
   });
 
-  it("shouldn't let users buy if they don't send enough eth", function() {
-    let KH;
-    return KetherHomepage.new(owner, withdrawWallet)
-      .then(function(instance) {
-        KH = instance;
-
-        return KH.buy(0, 0, 10, 10, { value: 1, from: account1 })
-      })
-      .then(function(returnValue) {
-        // This should not be hit since we threw an error
-        assert.fail();
-      })
-      .catch(function(error) {
-        // revert / require present as an invalid opcode error (at least in my environment)
-        assert(error.message.indexOf("reverted") >= 0, error.message);
-      });
+  it("shouldn't let users buy if they don't send enough eth", async function() {
+    await expect(
+      KH.connect(account1).buy(0, 0, 10, 10, { value: 1 })
+    ).to.be.revertedWith("Transaction reverted without a reason");
   });
 
-  it("should let users buy if they send enough eth", function() {
-    let KH;
-    let watcher;
-    let idx;
-    return KetherHomepage.new(owner, withdrawWallet)
-      .then(function(instance) {
-        KH = instance;
-        watcher = KH.Buy();
+  it("should let users buy if they send enough eth", async function() {
+    const result = await KH.connect(account1).buy(0, 0, 10, 10, { value: oneHundredCellPrice });
+    const receipt = await result.wait(); // Let events populate
 
-        return KH.buy(0, 0, 10, 10, { value: oneHundredCellPrice, from: account1 })
-      })
-      .then(function(result) {
-        // Make sure we issued the right Buy() event
-        assert.equal("Buy", result.logs[0].event);
-        const buyEvent = result.logs[0].args;
+    const events = await KH.queryFilter(KH.filters.Buy(), receipt.blockHash)
+    const buyEvent = events[0];
+    const idx = buyEvent.args.idx;
 
-        idx = buyEvent.idx;
+    // Make sure we issued the right Buy() event
+    expect(buyEvent.event).to.equal('Buy');
+    expect(idx).to.equal(0);
 
-        assert.equal(account1, buyEvent.owner)
-        assert.equal(0, buyEvent.x.toNumber());
-        assert.equal(0, buyEvent.y.toNumber());
-        assert.equal(10, buyEvent.width.toNumber());
-        assert.equal(10, buyEvent.height.toNumber());
+    expect(await account1.getAddress()).to.equal(buyEvent.args.owner);
+    expect(0).to.equal(buyEvent.args.x.toNumber());
+    expect(0).to.equal(buyEvent.args.y.toNumber());
+    expect(10).to.equal(buyEvent.args.width.toNumber());
+    expect(10).to.equal(buyEvent.args.height.toNumber());
 
-        // make sure the grid is filled
-        return KH.grid.call(0, 0);
-      })
-      .then(function(res) {
-        assert.equal(true, res);
+    // Make sure the grid is filled
+    expect(await KH.grid(0, 0)).to.be.true;
+    expect(await KH.grid(10, 0)).to.be.false;
 
-        return KH.grid.call(10, 10);
-      })
-      .then(function(res) {
-        assert.equal(false, res);
+    const ads = await KH.ads(idx);
 
-        return KH.ads.call(idx);
-      })
-      .then(function(ad) {
-        // Make sure we added the ad
-        assert.equal(account1, ad[0]);
-        assert.equal(0, ad[1].toNumber());
-        assert.equal(0, ad[2].toNumber());
-        assert.equal(10, ad[3].toNumber());
-        assert.equal(10, ad[4].toNumber());
-      })
+    // Make sure we added the ad
+    expect(await account1.getAddress()).to.equal(ads[0]);
+    expect(0).to.equal(ads[1].toNumber());
+    expect(0).to.equal(ads[2].toNumber());
+    expect(10).to.equal(ads[3].toNumber());
+    expect(10).to.equal(ads[4].toNumber());
   });
 
-  it("shouldn't let users buy overlapping adspace", function() {
-    let KH;
-    return KetherHomepage.new(owner, withdrawWallet)
-      .then(function(instance) {
-        KH = instance;
-
-        return KH.buy(0, 0, 10, 10, { value: oneHundredCellPrice, from: account1 })
-      })
-      .then(function() {
-        return KH.buy(5, 5, 10, 10, { value: oneHundredCellPrice, from: account2 })
-      })
-      .then(function() {
-        assert.fail();
-      })
-      .catch(function(error) {
-        assert(error.message.indexOf("reverted") >= 0, error.message);
-      })
+  it("shouldn't let users buy overlapping adspace", async function() {
+    await KH.connect(account1).buy(0, 0, 10, 10, { value: oneHundredCellPrice });
+    await expect(
+      KH.connect(account2).buy(5, 5, 10, 10, { value: oneHundredCellPrice })
+    ).to.be.revertedWith("Transaction reverted without a reason");
   });
 
-  it("should let a user publish their own ad", function() {
-    let KH;
-    return KetherHomepage.new(owner, withdrawWallet)
-      .then(function(instance) {
-        KH = instance;
-        return KH.buy(0, 0, 10, 10, { value: oneHundredCellPrice, from: account1 })
-      })
-      .then(function() {
-        return KH.publish(0, "link", "image", "title", false, { from: account1 })
-      })
-      .then(function() {
-        return KH.ads.call(0);
-      })
-      .then(function(ad) {
-        // Make sure we added the ad
-        assert.equal(account1, ad[0]);
-        assert.equal(0, ad[1].toNumber());
-        assert.equal(0, ad[2].toNumber());
-        assert.equal(10, ad[3].toNumber());
-        assert.equal(10, ad[4].toNumber());
-        assert.equal("link", ad[5]);
-        assert.equal("image", ad[6]);
-        assert.equal("title", ad[7]);
-        assert.equal(false, ad[8]);
-      });
+  it("should let a user publish their own ad", async function() {
+    await KH.connect(account1).buy(0, 0, 10, 10, { value: oneHundredCellPrice });
+    await KH.connect(account1).publish(0, "link", "image", "title", false)
+    const ad = await KH.ads(0);
+
+    // Make sure we added the ad
+    expect(await account1.getAddress()).to.equal(ad[0]);
+    expect(0).to.equal(ad[1].toNumber());
+    expect(0).to.equal(ad[2].toNumber());
+    expect(10).to.equal(ad[3].toNumber());
+    expect(10).to.equal(ad[4].toNumber());
+    expect("link").to.equal(ad[5]);
+    expect("image").to.equal(ad[6]);
+    expect("title").to.equal(ad[7]);
+    expect(false).to.equal(ad[8]);
   });
 
-  it("shouldn't let a user publish another user's ad", function() {
-    let KH;
-    return KetherHomepage.new(owner, withdrawWallet)
-      .then(function(instance) {
-        KH = instance;
-
-        return KH.buy(0, 0, 10, 10, { value: oneHundredCellPrice, from: account1 })
-      })
-      .then(function() {
-        return KH.publish(0, "link", "image", "title", false, { from: account2 })
-      })
-      .then(function() {
-        assert.fail();
-      })
-      .catch(function(error) {
-        assert(error.message.indexOf("reverted") >= 0, error.message);
-      })
+  it("shouldn't let a user publish another user's ad", async function() {
+    await KH.connect(account1).buy(0, 0, 10, 10, { value: oneHundredCellPrice });
+    await expect(
+      KH.connect(account2).publish(0, "link", "image", "title", false)
+    ).to.be.revertedWith("Transaction reverted without a reason");
   });
 
-  it("should let the contract owner forceNSFW", function() {
+  xit("should let the contract owner forceNSFW", function() {
     let KH;
     return KetherHomepage.new(owner, withdrawWallet)
       .then(function(instance) {
@@ -199,7 +133,7 @@ describe('KetherHomepage', function(accounts) {
       })
   });
 
-  it("shouldn't let non contract owners forceNSFW", function() {
+  xit("shouldn't let non contract owners forceNSFW", function() {
     let KH;
     return KetherHomepage.new(owner, withdrawWallet)
       .then(function(instance) {
@@ -218,7 +152,7 @@ describe('KetherHomepage', function(accounts) {
       })
   });
 
-  it("should let the owner withdraw", function() {
+  xit("should let the owner withdraw", function() {
     let initialBalance;
     let newBalance;
     let KH;
@@ -244,7 +178,7 @@ describe('KetherHomepage', function(accounts) {
       })
   });
 
-  it("shouldn't let non-owners withdraw", function() {
+  xit("shouldn't let non-owners withdraw", function() {
     let KH;
     return KetherHomepage.new(owner, withdrawWallet)
       .then(function(instance) {
@@ -263,7 +197,7 @@ describe('KetherHomepage', function(accounts) {
       })
   });
 
-  it("shouldn't let users buy 0-width space", function() {
+  xit("shouldn't let users buy 0-width space", function() {
     return KetherHomepage.new(owner, withdrawWallet)
       .then(function(instance) {
         return instance.buy(0, 0, 10, 0, { value: oneHundredCellPrice, from: account1 })
@@ -278,7 +212,7 @@ describe('KetherHomepage', function(accounts) {
       });
   });
 
-  it("shouldn't let users buy negative space", function() {
+  xit("shouldn't let users buy negative space", function() {
     return KetherHomepage.new(owner, withdrawWallet)
       .then(function(instance) {
         return instance.buy(30, 30, -10, -10, { value: oneHundredCellPrice, from: account1 })
@@ -293,7 +227,7 @@ describe('KetherHomepage', function(accounts) {
       });
   });
 
-  it("shouldn't let users buy out of bounds space", function() {
+  xit("shouldn't let users buy out of bounds space", function() {
     return KetherHomepage.new(owner, withdrawWallet)
       .then(function(instance) {
         return instance.buy(99, 99, 10, 10, { value: oneHundredCellPrice, from: account1 })
@@ -308,7 +242,7 @@ describe('KetherHomepage', function(accounts) {
       });
   });
 
-  it("should let a user setAdOwner on their own ad", function() {
+  xit("should let a user setAdOwner on their own ad", function() {
     let KH;
     return KetherHomepage.new(owner, withdrawWallet)
       .then(function(instance) {
@@ -340,7 +274,7 @@ describe('KetherHomepage', function(accounts) {
       });
   });
 
-  it("shouldn't let a user setAdOwner on another user's ad", function() {
+  xit("shouldn't let a user setAdOwner on another user's ad", function() {
     let KH;
     return KetherHomepage.new(owner, withdrawWallet)
       .then(function(instance) {
