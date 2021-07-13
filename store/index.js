dd30df2281aebf9d46b91c1aa9b05c38c7e3dba3
Andrey Petrov <andrey.petrov@shazow.net>
2021-07-13 12:14:19 -0700

    store/index.js: Store truthy grid cells as 1 to make initState smaller

--- store/index.js
+++ store/index.js
@@ -258,7 +258,7 @@ function filledGrid(grid, ads) {
 function setBox(grid, x1, y1, x2, y2) {
   for (let x = Number(x1); x <= x2; x++) {
     for (let y = Number(y1); y <= y2; y++) {
-      grid[x][y] = true;
+      grid[x][y] = 1;
     }
   }
 }
