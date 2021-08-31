d1fcca8c3ca97b467ce876bd48e2547ff0a3a2e6
Andrey Petrov <andrey.petrov@shazow.net>
2021-08-31 09:11:37 -0400

    components/Footer.vue: Add discord and twitter

--- components/Footer.vue
+++ components/Footer.vue
@@ -10,6 +10,8 @@
       <li><NuxtLink to="faq">FAQ</NuxtLink></li>
       <li><NuxtLink to="press">Press</NuxtLink></li>
       <li><a href="https://github.com/thousandetherhomepage">Source code</a></li>
+      <li><a href="https://twitter.com/ketherhomepage">Twitter</a></li>
+      <li><a href="https://discord.gg/vHCNE5nfQc">Discord</a></li>
     </ul>
     <slot></slot>
   </footer>
