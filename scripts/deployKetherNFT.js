2d8d7bdb2555c1ebfccff49b88ea0382c2faafa5
Andrey Petrov <andrey.petrov@shazow.net>
2021-09-01 09:39:45 -0400

    scripts/deployKetherNFT.js: EIP1559 gas fee

--- scripts/deployKetherNFT.js
+++ scripts/deployKetherNFT.js
@@ -5,12 +5,16 @@ const deployed = {
   'rinkeby': {
     ownerAddress: "0xbCb061d2feE38DCB6DE7e5D269852B4BDb986Ed6",
     ketherHomepageAddress: "0xb88404dd8fe4969ef67841250baef7f04f6b1a5e",
+    ketherNFTOwnerAddress: "0xbCb061d2feE38DCB6DE7e5D269852B4BDb986Ed6",
     ketherNFTRendererAddress: "0xc767472dddaa5eb84e4dcc237101ae7fd7f2e03c",
     ketherNFTAddress: "0xB7fCb57a5ce2F50C3203ccda27c05AEAdAF2C221",
   },
   'mainnet': {
     ownerAddress: "0xd534d9f6e61780b824afaa68032a7ec11720ca12",
+    ketherNFTOwnerAddress: "0x714439382A47A23f7cdF56C9764ec22943f79361",
     ketherHomepageAddress: "0xb5fe93ccfec708145d6278b0c71ce60aa75ef925",
+    ketherNFTRendererAddress: "0x228c17030a866CcBf6734fA4262Dee64f0E392be",
+    ketherNFTAddress: "0x7bb952AB78b28a62b1525acA54A71E7Aa6177645",
   },
 };
 
@@ -25,8 +29,19 @@ async function main() {
   }
 
   if (network.name !== 'rinkeby') {
-    throw "Only rinkeby allowed by default";
+     throw "Only rinkeby allowed by default";
   }
+
+  const feeData = await ethers.provider.getFeeData();
+  console.log("Current fee data", ethers.utils.formatUnits(feeData.maxPriorityFeePerGas, "gwei"), ethers.utils.formatUnits(feeData.maxFeePerGas, "gwei"));
+
+  const gasPrice = await ethers.provider.getGasPrice();
+  console.log("Current gas price", ethers.utils.formatUnits(gasPrice, "gwei"));
+
+  const maxFeePerGas = feeData.maxFeePerGas;
+  const maxPriorityFeePerGas = feeData.maxPriorityFeePerGas;
+  //const maxFeePerGas = ethers.utils.parseUnits("80" , "gwei")
+  //const maxPriorityFeePerGas = ethers.utils.parseUnits("2", "gwei");
 
   // Confirm the contract is actually there
   const KH = await ethers.getContractAt("KetherHomepage", cfg.ketherHomepageAddress);
@@ -34,8 +49,8 @@ async function main() {
   const [account] = await ethers.getSigners();
   if (account === undefined) {
     throw "Signer account not provided, specify ACCOUNT_PRIVATE_KEY";
-  } else if (account.address !== cfg.ownerAddress) {
-    throw "Did not acquire signer for owner address: " + cfg.ownerAddress + "; got: " + account.address;
+  } else if (account.address !== cfg.ketherNFTOwnerAddress) {
+    throw "Did not acquire signer for owner address: " + cfg.ketherNFTOwnerAddress + "; got: " + account.address;
   }
   console.log("Starting deploys from address:", account.address);
 
@@ -44,7 +59,7 @@ async function main() {
 
   let ketherNFTRendererAddress = cfg["ketherNFTRendererAddress"];
   if (ketherNFTRendererAddress === undefined) {
-    const KNFTrender = await KetherNFTRender.deploy();
+    const KNFTrender = await KetherNFTRender.deploy({ maxFeePerGas, maxPriorityFeePerGas });
     console.log("Deploying KetherNFTRender to:", KNFTrender.address);
     ketherNFTRendererAddress = KNFTrender.address;
 
@@ -58,7 +73,7 @@ async function main() {
 
   let ketherNFTAddress = cfg["ketherNFTAddress"];
   if (ketherNFTAddress === undefined) {
-    const KNFT = await KetherNFT.deploy(KH.address, ketherNFTRendererAddress);
+    const KNFT = await KetherNFT.deploy(KH.address, ketherNFTRendererAddress, { maxFeePerGas, maxPriorityFeePerGas });
     console.log("Deploying KetherNFT to:", KNFT.address);
     ketherNFTAddress = KNFT.address;
 
