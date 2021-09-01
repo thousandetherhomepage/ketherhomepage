7ec7430b9b794a55aebe001e1025b8812ada2b69
Max Veytsman <maxim@ontoillogical.com>
2021-09-01 18:25:21 -0400

    deploy KetherView

--- scripts/deployKetherNFT.js
+++ scripts/deployKetherNFT.js
@@ -8,6 +8,7 @@ const deployed = {
     ketherNFTOwnerAddress: "0xbCb061d2feE38DCB6DE7e5D269852B4BDb986Ed6",
     ketherNFTRendererAddress: "0xc767472dddaa5eb84e4dcc237101ae7fd7f2e03c",
     ketherNFTAddress: "0xB7fCb57a5ce2F50C3203ccda27c05AEAdAF2C221",
+    ketherViewAddress: "0x126C76281Fb6ee945BeF9b92aaC5D46eB8bDA299"
   },
   'mainnet': {
     ownerAddress: "0xd534d9f6e61780b824afaa68032a7ec11720ca12",
@@ -50,12 +51,14 @@ async function main() {
   if (account === undefined) {
     throw "Signer account not provided, specify ACCOUNT_PRIVATE_KEY";
   } else if (account.address !== cfg.ketherNFTOwnerAddress) {
-    throw "Did not acquire signer for owner address: " + cfg.ketherNFTOwnerAddress + "; got: " + account.address;
+   // throw "Did not acquire signer for owner address: " + cfg.ketherNFTOwnerAddress + "; got: " + account.address;
   }
   console.log("Starting deploys from address:", account.address);
 
   const KetherNFT = await ethers.getContractFactory("KetherNFT");
   const KetherNFTRender = await ethers.getContractFactory("KetherNFTRender");
+  const KetherView = await ethers.getContractFactory("KetherView");
+
 
   let ketherNFTRendererAddress = cfg["ketherNFTRendererAddress"];
   if (ketherNFTRendererAddress === undefined) {
@@ -84,6 +87,20 @@ async function main() {
   }
 
   console.log(`Verify on Etherscan: npx hardhat verify --network ${network.name} ${ketherNFTAddress} "${KH.address}" "${ketherNFTRendererAddress}"`);
+
+  let ketherViewAddress = cfg["ketherViewAddress"];
+  if (ketherViewAddress === undefined) {
+    const KView = await KetherView.deploy({ maxFeePerGas, maxPriorityFeePerGas });
+    console.log("Deploying KetherView to:", KView.address);
+    ketherViewAddress = KView.address;
+
+    const tx = await KView.deployTransaction.wait();
+    console.log(" -> Mined with", tx.gasUsed.toString(), "gas");
+  } else {
+    console.log("KetherView already deployed");
+  }
+
+  console.log(`Verify on Etherscan: npx hardhat verify --network ${network.name} ${ketherViewAddress}`);
 }
 
 main()
