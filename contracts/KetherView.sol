cd42e9c74c057920d953e6c3b383ba22056924ee
Max Veytsman <maxim@ontoillogical.com>
2021-09-01 13:03:22 -0400

    KetherView library

--- contracts/KetherView.sol
+++ contracts/KetherView.sol
@@ -1,29 +1,39 @@
 import "./IKetherHomepage.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract KetherView {
+library KetherView {
+  struct AdView {
+    address owner;
+    uint x;
+    uint y;
+    uint width;
+    uint height;
+    string link;
+    string image;
+    string title;
+    bool NSFW;
+    bool forceNSFW;
+    bool wrapped;
+  }
   /// allAds is a helper view designed to be called from frontends that want to
   /// display all of the ads with their correct NFT owners.
-  function allAds(IKetherHomepage instance) external view returns (IKetherHomepage.Ad[] memory) {
+  function allAds(address _instanceAddress, address _nftAddress) external view returns (AdView[] memory) {
     // FIXME: Does this actually enumerate all of the ads, or do we need to paginate?
-    uint len = instance.getAdsLength();
-    IKetherHomepage.Ad[] memory ads_ = new IKetherHomepage.Ad[](len);
+    AdView[] memory ads_ = new AdView[](IKetherHomepage(_instanceAddress).getAdsLength());
 
-    for (uint idx=0; idx<len; idx++) {
-      (address owner, uint x, uint y, uint width, uint height, string memory link, string memory image, string memory title, bool NSFW, bool forceNSFW) = instance.ads(idx);
-
-      /* XXX: Fix this
+    for (uint idx=0; idx<ads_.length; idx++) {
+      (address owner, uint x, uint y, uint width, uint height, string memory link, string memory image, string memory title, bool NSFW, bool forceNSFW) = IKetherHomepage(_instanceAddress).ads(idx);
+      bool wrapped = false;
 
       // Is it an NFT already?
-      if (_exists(idx)) {
+      if (owner == _nftAddress) {
         // Override owner to be the NFT owner
-        owner = ownerOf(idx);
+        owner = IERC721(_nftAddress).ownerOf(idx);
+        wrapped = true;
       }
 
-      */
-
-      ads_[idx] = IKetherHomepage.Ad(owner, x, y, width, height, link, image, title, NSFW, forceNSFW);
+      ads_[idx] = AdView(owner, x, y, width, height, link, image, title, NSFW, forceNSFW, wrapped);
     }
     return ads_;
   }
-
 }
---
+++ test/ketherview.js
@@ -0,0 +1,71 @@
+const { expect } = require('chai');
+
+const weiPixelPrice = ethers.utils.parseUnits("0.001", "ether");
+const pixelsPerCell = ethers.BigNumber.from(100);
+const oneHundredCellPrice = pixelsPerCell.mul(weiPixelPrice).mul(100);
+
+describe('KetherView', function() {
+  let KetherHomepage, KetherNFT, KetherView;
+  let accounts, KH, KNFT;
+
+  beforeEach(async () => {
+    // NOTE: We're using V2 here because it's ported to newer solidity so we can debug more easily. It should also work with V1.
+    KetherHomepage = await ethers.getContractFactory("KetherHomepageV2");
+    KetherNFT = await ethers.getContractFactory("KetherNFT");
+    KetherNFTRender = await ethers.getContractFactory("KetherNFTRender");
+    KetherView = await ethers.getContractFactory("KetherView");
+
+
+    const [owner, withdrawWallet, metadataSigner, account1, account2, account3] = await ethers.getSigners();
+    accounts = {owner, withdrawWallet, metadataSigner, account1, account2, account3};
+
+    KH = await KetherHomepage.deploy(await owner.getAddress(), await withdrawWallet.getAddress());
+    KNFTrender = await KetherNFTRender.deploy();
+    KNFT = await KetherNFT.deploy(KH.address, KNFTrender.address);
+    KV = await KetherView.deploy();
+
+  });
+
+  const buyAd = async function(account, x=0, y=0, width=10, height=10, link="link", image="image", title="title", NSFW=false, value=oneHundredCellPrice) {
+    const txn = await KH.connect(account).buy(x, y, width, height, { value: value });
+    const receipt = await txn.wait();
+    const event = receipt.events.pop();
+    const [idx] = event.args;
+    await KH.connect(account).publish(idx, link, image, title, false);
+    return idx;
+  }
+
+  it("it should return all of the ads as a helper", async function() {
+    const {
+      owner,
+      account1
+    } = accounts;
+
+    // Buy an ad
+    const idx = await buyAd(account1);
+
+    // One more
+    const idx2 = await buyAd(account1, x = 20, y = 20);
+    expect(idx2).to.equal(1);
+
+    // Wrap the second ad
+    const [salt, precomputeAddress] = await KNFT.connect(account1).precompute(idx2, await account1.getAddress());
+
+    // Set owner to precommitted wrap address
+    await KH.connect(account1).setAdOwner(idx2, precomputeAddress);
+
+    // Wrap ad
+    await KNFT.connect(account1).wrap(idx2, await account1.getAddress());
+
+    const ads = await KV.connect(account1).allAds(KH.address, KNFT.address);
+    expect(ads).to.to.have.lengthOf(2);
+
+    // first ad has us as owner and is not wrapped
+    expect(ads[0].owner).to.equal(account1.address)
+    expect(ads[0].wrapped).to.equal(false)
+
+    // second ad has us as owner and is wrapped
+    expect(ads[1].owner).to.equal(account1.address)
+    expect(ads[1].wrapped).to.equal(true)
+ });
+});
