84f2a724dea2bc14698ddca1e41e2be4bf1e0015
Max Veytsman <maxim@ontoillogical.com>
2021-09-14 10:22:55 -0400

    Add upgrade-insecure-requests to CSP

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,7 @@ export default {
       { charset: 'utf-8' },
       //{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: "The Million Dollar Homepage as an Ethereum Smart Contract and DApp: A glimpse into what the future of web integrated with modern blockchain technology could be like." },
-      process.env.NODE_ENV === 'production' ? { 'http-equiv': "Content-Security-Policy", content: "default-src 'self'; script-src 'self' 'self' *.infura.io; connect-src 'self' *.infura.io *.walletconnect.org wss://*.walletconnect.org; style-src 'self' 'unsafe-inline'; img-src * data:"} : {},
+      process.env.NODE_ENV === 'production' ? { 'http-equiv': "Content-Security-Policy", content: "upgrade-insecure-requests; default-src 'self'; script-src 'self' 'self' *.infura.io; connect-src 'self' *.infura.io *.walletconnect.org wss://*.walletconnect.org; style-src 'self' 'unsafe-inline'; img-src * data:"} : {},
       { name: "twitter:card", content: "summary"},
       { property: "og:url", content: "https://thousandetherhomepage.com"},
       { property: "og:title", content: "The Thousand Ether Homepage" },
