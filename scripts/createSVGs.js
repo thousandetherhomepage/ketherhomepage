5647afa007a131ddc71cd9c6baa5bd8fcf85d488
Andrey Petrov <andrey.petrov@shazow.net>
2021-11-04 11:58:32 -0400

    scripts/createSVGs.js: Tweak rendering paths, fix KetherView Rinkeby, add logs

--- scripts/createSVGs.js
+++ scripts/createSVGs.js
@@ -10,7 +10,7 @@ const deployed = {
     ketherNFTOwnerAddress: "0xbCb061d2feE38DCB6DE7e5D269852B4BDb986Ed6",
     ketherNFTRendererAddress: "0xc767472dddaa5eb84e4dcc237101ae7fd7f2e03c",
     ketherNFTAddress: "0xB7fCb57a5ce2F50C3203ccda27c05AEAdAF2C221",
-    ketherViewAddress: "0x126C76281Fb6ee945BeF9b92aaC5D46eB8bDA299",
+    ketherViewAddress: "0xd58d4ff574140472f9ae2a90b6028df822c10109",
     ketherSortitionAddress: "0xA194a30C201523631E29EFf80718D72994eFa1d6",
   },
   'mainnet': {
@@ -31,9 +31,8 @@ const networkNames = {
 }
 
 async function main() {
-
   const network = await ethers.provider.getNetwork();
-  const networkName = networkNames[network.name]
+  const networkName = networkNames[network.name];
 
   const cfg = deployed[networkName];
 
@@ -41,12 +40,14 @@ async function main() {
     throw "Unsupported network: "+ network.name;
   }
 
-  const grid = await fs.readFile(`${__dirname}/../static/${networkName}-grid.svg`, "utf-8");
+  const basePath = `${__dirname}/../../kethernft-images/${networkName}`;
+
+  const grid = await fs.readFile(`${basePath}/grid.svg`, "utf-8");
 
   const KetherHomepage = await ethers.getContractFactory("KetherHomepageV2");
   const KetherView = await ethers.getContractFactory("KetherView");
 
-  const KH = await KetherHomepage.attach(cfg.ketherHomepageAddress)
+  const KH = await KetherHomepage.attach(cfg.ketherHomepageAddress);
   const KV = await KetherView.attach(cfg.ketherViewAddress);
 
   const len = await KH.getAdsLength();
@@ -54,10 +55,13 @@ async function main() {
   for (let offset=0; offset < len; offset += chunkSize) {
     // TODO: this fails for Rinkeby with
     // Error: cannot estimate gas; transaction may fail or may require manual gas limit (error={"name":"ProviderError","code":-32000,"_isProviderError":true}, method="call", transaction={"from":"0xd4cDdaD41Ba3475Fb5573aCbE0757cE1c15F1374","to":"0x126C76281Fb6ee945BeF9b92aaC5D46eB8bDA299","data":"0xa96cf3ea000000000000000000000000b88404dd8fe4969ef67841250baef7f04f6b1a5e000000000000000000000000b7fcb57a5ce2f50c3203ccda27c05aeadaf2c22100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001","accessList":null}, code=UNPREDICTABLE_GAS_LIMIT, version=providers/5.4.5)
+    console.log("KV.allAds", cfg.ketherHomepageAddress, cfg.ketherNFTAddress, offset, chunkSize);
     let ads = await KV.allAds(cfg.ketherHomepageAddress, cfg.ketherNFTAddress, offset, chunkSize);
 
     for (ad of ads) {
-      await fs.writeFile(`${__dirname}/../static/${networkName}/${ad.idx}.svg`, svg(grid,ad));
+      const path = `${basePath}/${ad.idx}.svg`;
+      console.log("Generating:", path);
+      await fs.writeFile(path, svg(grid, ad));
     }
   }
 }
