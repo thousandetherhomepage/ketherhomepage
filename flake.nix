fd2dd10e76d6a0e9a9b1af32fe973018e69cde73
Andrey Petrov <andrey.petrov@shazow.net>
2023-06-03 11:25:45 -0700

    Switch from shell.nix -> flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    foundry.url = "github:shazow/foundry.nix/monthly"; # Use monthly branch for permanent releases
+  };
+
+  outputs = { self, nixpkgs, utils, foundry }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ foundry.overlay ];
+        };
+      in {
+
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            nodejs-18_x
+            nodePackages.npm
+
+            foundry-bin
+          ];
+
+          shellHook = ''
+            export PS1="[dev] $PS1"
+            export PATH=$PWD/node_modules/.bin:$PATH
+          '';
+        };
+      });
+}
